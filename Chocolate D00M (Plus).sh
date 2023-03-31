0fd1d3ef6f643143ff5b6ad3dd154ba0257cf5e9
Rapid Edwin <88399538+RapidEdwin08@users.noreply.github.com>
2023-03-30 22:35:02 -0400

    Update Chocolate D00M (Plus).sh

--- Chocolate D00M (Plus).sh
+++ Chocolate D00M (Plus).sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # https://github.com/RapidEdwin08/chocolate-doom-plus v2023.03
 
-"/opt/retropie/supplementary/runcommand/runcommand.sh" 0 _PORT_ "chocolate-doom-plus" "$HOME/RetroPie/roms/ports/doom/doom2.wad -file $HOME/RetroPie/roms/ports/doom/mods/Maps/KDIKDIZD/KDiKDi_A.wad $HOME/RetroPie/roms/ports/doom/mods/Maps/KDIKDIZD/KDiKDi_B.wad -deh $HOME/RetroPie/roms/ports/doom/mods/Maps/KDIKDIZD/kdikdizd.deh"
+"/opt/retropie/supplementary/runcommand/runcommand.sh" 0 _PORT_ "chocolate-doom-plus" "$HOME/RetroPie/roms/ports/doom/doom2.wad -file $HOME/RetroPie/roms/ports/doom/mods/PGDEMO19.WAD $HOME/RetroPie/roms/ports/doom/mods/PG-LTD-X.WAD"
