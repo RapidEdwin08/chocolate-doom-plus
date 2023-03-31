b3f9dbddcdfc7e13111a1d701d90d2743280d509
Rapid Edwin <88399538+RapidEdwin08@users.noreply.github.com>
2023-03-31 14:27:24 -0400

    Update Chocolate D00M (Plus).sh

--- Chocolate D00M (Plus).sh
+++ Chocolate D00M (Plus).sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 # https://github.com/RapidEdwin08/chocolate-doom-plus v2023.03
 
-"/opt/retropie/supplementary/runcommand/runcommand.sh" 0 _PORT_ "chocolate-doom-plus" "$HOME/RetroPie/roms/ports/doom/doom2.wad -file $HOME/RetroPie/roms/ports/doom/mods/PerditionsGate/PG-LTD.WAD $HOME/RetroPie/roms/ports/doom/mods/PerditionsGate/PGDEMO19.WAD"
+"/opt/retropie/supplementary/runcommand/runcommand.sh" 0 _PORT_ "chocolate-doom-plus" "$HOME/RetroPie/roms/ports/doom/doom1.wad"
+
+# EXAMPLES:
+## _PORT_ "chocolate-doom-plus" "$HOME/RetroPie/roms/ports/doom/doomu.wad"
+## _PORT_ "chocolate-doom-plus" "$HOME/RetroPie/roms/ports/doom/tnt.wad"
+## _PORT_ "chocolate-doom-plus" "$HOME/RetroPie/roms/ports/doom/plutonia.wad"
+## _PORT_ "chocolate-doom-plus" "$HOME/RetroPie/roms/ports/doom/doom2.wad"
+## _PORT_ "chocolate-doom-plus" "$HOME/RetroPie/roms/ports/doom/heretic.wad"
+## _PORT_ "chocolate-doom-plus" "$HOME/RetroPie/roms/ports/doom/heretic.wad -warp 6 1 -skill 3"
+## _PORT_ "chocolate-doom-plus" "$HOME/RetroPie/roms/ports/doom/hexen.wad"
+## _PORT_ "chocolate-doom-plus" "$HOME/RetroPie/roms/ports/doom/hexen.wad -file $HOME/RetroPie/roms/ports/doom/hexdd.wad"
+## _PORT_ "chocolate-doom-plus" "$HOME/RetroPie/roms/ports/doom/strife1.wad -file $HOME/RetroPie/roms/ports/doom/voices.wad"
+## _PORT_ "chocolate-doom-plus" "$HOME/RetroPie/roms/ports/doom/doom2.wad -file $HOME/RetroPie/roms/ports/doom/french.wad -deh $HOME/RetroPie/roms/ports/doom/french.deh"
+## _PORT_ "chocolate-doom-plus" "$HOME/RetroPie/roms/ports/doom/doom2.wad -merge $HOME/RetroPie/roms/ports/doom/mods/Hell2Pay/HTP-LTD.WAD $HOME/RetroPie/roms/ports/doom/mods/Hell2Pay/HTPDMO19.WAD"
+## _PORT_ "chocolate-doom-plus" "$HOME/RetroPie/roms/ports/doom/doom2.wad -file $HOME/RetroPie/roms/ports/doom/mods/PerditionsGate/PG-LTD.WAD $HOME/RetroPie/roms/ports/doom/mods/PerditionsGate/PGDEMO19.WAD"
+## _PORT_ "chocolate-doom-plus" "$HOME/RetroPie/roms/ports/doom/doom2.wad -file $HOME/RetroPie/roms/ports/doom/mods/KDIKDIZD/KDiKDi_A.wad $HOME/RetroPie/roms/ports/doom/mods/KDIKDIZD/KDiKDi_B.wad -deh $HOME/RetroPie/roms/ports/doom/mods/KDIKDIZD/kdikdizd.deh"
