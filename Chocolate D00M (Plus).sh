2e85051a7a77524668e0a3a35ac93399c5b469e9
Rapid Edwin <88399538+RapidEdwin08@users.noreply.github.com>
2023-07-06 21:15:45 -0400

    Update Chocolate D00M (Plus).sh

--- Chocolate D00M (Plus).sh
+++ Chocolate D00M (Plus).sh
@@ -12,7 +12,7 @@
 ## _PORT_ "chocolate-doom-plus" "$HOME/RetroPie/roms/ports/doom/heretic.wad -warp 6 1 -skill 3"
 ## _PORT_ "chocolate-doom-plus" "$HOME/RetroPie/roms/ports/doom/hexen.wad"
 ## _PORT_ "chocolate-doom-plus" "$HOME/RetroPie/roms/ports/doom/hexen.wad -file $HOME/RetroPie/roms/ports/doom/hexdd.wad"
-## _PORT_ "chocolate-doom-plus" "$HOME/RetroPie/roms/ports/doom/strife1.wad -file $HOME/RetroPie/roms/ports/doom/voices.wad"
+## _PORT_ "chocolate-doom-plus" "$HOME/RetroPie/roms/ports/doom/strife1.wad -merge $HOME/RetroPie/roms/ports/doom/voices.wad"
 ## _PORT_ "chocolate-doom-plus" "$HOME/RetroPie/roms/ports/doom/doom2.wad -file $HOME/RetroPie/roms/ports/doom/french.wad -deh $HOME/RetroPie/roms/ports/doom/french.deh"
 ## _PORT_ "chocolate-doom-plus" "$HOME/RetroPie/roms/ports/doom/doom2.wad -merge $HOME/RetroPie/roms/ports/doom/mods/Hell2Pay/HTP-LTD.WAD $HOME/RetroPie/roms/ports/doom/mods/Hell2Pay/HTPDMO19.WAD"
 ## _PORT_ "chocolate-doom-plus" "$HOME/RetroPie/roms/ports/doom/doom2.wad -file $HOME/RetroPie/roms/ports/doom/mods/PerditionsGate/PG-LTD-X.WAD $HOME/RetroPie/roms/ports/doom/mods/PerditionsGate/PGDEMO19.WAD"
